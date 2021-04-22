068f5bf804173528596dc6ec93af207fb9ccb2de
EmmanueleLibraro <emmanuelelibraro95@gmail.com>
2021-04-22 16:24:00 +0200

    text

---
+++ JS/script.js
@@ -0,0 +1,26 @@
+//VUE TODO LIST
+const app = new Vue({
+    el: '#app',
+    data: {
+        todos: [
+            {
+                text: 'Aspettare la closed beta',
+                completed: false,
+            },
+            {
+                text: 'Nerdare fino allo sfinimento',
+                completed: false,
+            },
+            {
+                text: 'Divertirsi',
+                completed: false,
+            }
+        ],
+        newTodo: '',
+        methods: {
+            addTodo(){
+                console.log('ADD new click');
+            }
+        }
+    }
+});

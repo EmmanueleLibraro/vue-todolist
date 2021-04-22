c1ca1f47326eb5ec72b58663b79e6475814f9c4c
EmmanueleLibraro <emmanuelelibraro95@gmail.com>
2021-04-22 16:42:56 +0200

    mi da errore

--- JS/script.js
+++ JS/script.js
@@ -19,8 +19,25 @@ const app = new Vue({
         newTodo: '',
     },
     methods: {
+        //INSERIRE TO DO NELLA LISTA
         addTodo(){
-            console.log('ADD new click');
+            //console.log('ADD new click');
+            if(this.new.Todo !== ''){
+                this.todos.push({
+                    text: this.newTodo,
+                    completed: false,
+                });
+
+                this.newTodo = '',
+                this.$refs.todoInput.focus();
+            }
+        },
+        //RIMUOVERE TO DO
+        removeTodo(index){
+            this.todos.splice(index, 1);
+        },
+        updateStatus(index){
+            this.todos[index].completed = ! this.todos[index].completed;
         }
     },
 });
